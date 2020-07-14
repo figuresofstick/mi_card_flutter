d48aa0dbf46b0472ad2294497680f5e7e6a4535f
Christopher Norris <chris.norris90@gmail.com>
2020-07-14 15:28:59 +1000

    Add contact details

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,39 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              SizedBox(
+                height: 20.0,
+                width: 150,
+                child: Divider(color: Colors.teal.shade100),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.teal),
+                  title: Text(
+                    '0499 199 027',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.email, color: Colors.teal),
+                  title: Text(
+                    'chris.norris90@outlook.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
@@ -43,3 +76,83 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// demo code (buttons)
+/*
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      FlatButton(
+                        color: Color(0xffB2DFDB),
+                        onPressed: () => {},
+                        child: Column(
+                          children: <Widget>[
+                            Icon(Icons.perm_contact_calendar,
+                                color: Color(0xff212121)),
+                            Text(
+                              'Update Details',
+                              style: TextStyle(
+                                color: Color(0xff212121),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      FlatButton(
+                        color: Color(0xffB2DFDB),
+                        onPressed: () => {},
+                        child: Column(
+                          children: <Widget>[
+                            Icon(Icons.payment, color: Color(0xff212121)),
+                            Text(
+                              'Payslips',
+                              style: TextStyle(
+                                color: Color(0xff212121),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      FlatButton(
+                        color: Color(0xffB2DFDB),
+                        onPressed: () => {},
+                        child: Column(
+                          children: <Widget>[
+                            Icon(Icons.assignment, color: Color(0xff212121)),
+                            Text(
+                              'Roster/Timesheet',
+                              style: TextStyle(
+                                color: Color(0xff212121),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      FlatButton(
+                        color: Color(0xffB2DFDB),
+                        onPressed: () => {},
+                        child: Column(
+                          children: <Widget>[
+                            Icon(Icons.flight, color: Color(0xff212121)),
+                            Text(
+                              'Leave Request',
+                              style: TextStyle(
+                                color: Color(0xff212121),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+*/
